ec6e5c46a8f0d40da06ec52ec74d97d3607a1fe9
mcproger <mcproger7@gmail.com>
2021-02-13 15:33:47 +0300

    Fix test expected values

--- src/tinkoff/tests/test_unit/test_helpers.py
+++ src/tinkoff/tests/test_unit/test_helpers.py
@@ -4,21 +4,21 @@ import pytest
 from tinkoff.helpers import operations_piechar_url
 
 
-@pytest.mark.freeze_time(datetime(2020, 1, 28))
+@pytest.mark.freeze_time(datetime(2020, 1, 28, 3, 0, 0))
 @pytest.mark.parametrize(
     ('from_datetime', 'to_datetime', 'expected_result'),
     [
         (
             None, None,
-            'https://www.tinkoff.ru/api/common/v1/operations_piechart?end=1580148000000&'
-            'groupBy=spendingCategory&notInner=true&start=1577901600000&type=Debit'
+            'https://www.tinkoff.ru/api/common/v1/operations_piechart?end=1580169600000&'
+            'groupBy=spendingCategory&notInner=true&start=1577836800000&type=Debit'
             '&sessionid=test-session&wuid=af3b1c996a3e2678fb05a25fab603e17',
         ),
         (
-            datetime(2021, 1, 1),
-            datetime(2021, 4, 1),
-            'https://www.tinkoff.ru/api/common/v1/operations_piechart?end=1617224400000&'
-            'groupBy=spendingCategory&notInner=true&start=1609448400000&type=Debit'
+            datetime(2021, 1, 1, 3, 0, 0),
+            datetime(2021, 4, 1, 3, 0, 0),
+            'https://www.tinkoff.ru/api/common/v1/operations_piechart?end=1617235200000&'
+            'groupBy=spendingCategory&notInner=true&start=1609459200000&type=Debit'
             '&sessionid=test-session&wuid=af3b1c996a3e2678fb05a25fab603e17',
         ),
     ],
